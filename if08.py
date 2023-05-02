7d265ee7a87f48f265e8caa4af33176405dee7da
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-02 23:30:13 +0500

    why none

--- if08.py
+++ if08.py
@@ -14,4 +14,21 @@ def main(number):
     Returns:
         str: return answer.
     """
+    if number == 1:
+        print('monday')
+    elif number==2:
+        print('tuesday')
+    elif number == 3:
+        print('wednesday')
+    elif number == 4:
+        print('thursday')
+    elif number ==5:
+        print('friday')
+    elif number == 6:
+        print('saturday')
+    elif number==7:
+        print('sunday')
     return
+    
+v = main(5)
+print(v)
