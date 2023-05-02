6ab271aa53e630e14df56edf76aa2dac04bdb3b9
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-02 21:28:44 +0500

    why none

--- if03.py
+++ if03.py
@@ -8,4 +8,13 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
+    if a<b and a>c:
+        print (a)
+    elif b<a and b>c:
+        print(b) 
+    elif c<a and c>b:
+        print (c)
     return
+
+v = main(5,4,1)
+print(v)
